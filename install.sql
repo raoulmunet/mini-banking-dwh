997ee6998f3a0b9f3ba0de03ef91b6386687fddf
raoulmunet <raoulmunet@gmail.com>
2026-09-23 20:04:18 +0300

    Add install.sql

---
+++ install.sql
@@ -0,0 +1,25 @@
+prompt Installing Mini Banking Data Warehouse...
+
+@sql/01_control_tables.sql
+@sql/02_staging_tables.sql
+@sql/03_dimension_tables.sql
+@sql/04_fact_tables.sql
+@sql/05_indexes.sql
+@sql/06_demo_source_data.sql
+@sql/07_dq_rules.sql
+@sql/09_lineage_objects.sql
+
+@plsql/pkg_batch.pks
+@plsql/pkg_batch.pkb
+@plsql/pkg_dq.pks
+@plsql/pkg_dq.pkb
+@plsql/pkg_recon.pks
+@plsql/pkg_recon.pkb
+@plsql/pkg_lineage.pks
+@plsql/pkg_lineage.pkb
+@plsql/pkg_dwh_load.pks
+@plsql/pkg_dwh_load.pkb
+
+@sql/08_reporting_views.sql
+
+prompt Installation complete.
